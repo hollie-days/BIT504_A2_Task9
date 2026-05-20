4ab047bed4e10372d67193bb7a198592f2ed73cd
Emma Holliday <emmajholliday@gmail.com>
2026-05-20 13:13:24 +1200

    added third output messages to Main class

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, this is my first commit!");
         System.out.println("BIT504 Assessment 2 - Task 9");
+        System.out.println("This is my second commit on a new branch!");
+        System.out.println("Learning about source and version control.");
+        System.out.println("This is my third commit on another new branch!");
+        System.out.println("Practicing merging and pull requests.");
     }
 }
