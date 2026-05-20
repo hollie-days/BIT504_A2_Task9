82e79e2d6e33eabc17ecf48b9ecaae9026336e5c
Emma Holliday <emmajholliday@gmail.com>
2026-05-20 12:38:46 +1200

    Initial commit - added Main class with welcome message

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// BIT504 - Assessment 2 - Task 9
+// Emma Holliday
+// 5118874
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is my first commit!");
+        System.out.println("BIT504 Assessment 2 - Task 9");
+    }
+}
